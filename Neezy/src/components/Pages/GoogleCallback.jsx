bcf6e9fdc3f0a01ea9e9c2e69f8fe203f9277a27
Choi, Sunghyeok <anstonchoi@gmail.com>
2025-08-19 21:26:46 +0900

    Feat: 구글 로그인 기능 추가

--- Neezy/src/components/Pages/GoogleCallback.jsx
+++ Neezy/src/components/Pages/GoogleCallback.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react;"
+import React, { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 
@@ -14,6 +14,27 @@ export default function GoogleCallback() {
             return;
         }
 
-        
-    })
+        console.log("구글 인가 코드:", code);
+
+        // const fetchGoogleToken = async () => {
+        //     try {
+        //         const response = await axios.post(
+        //             `${import.meta.env.VITE_BACKEND_URL}/auth/google`,
+        //             { code }
+        //         );
+
+        //         console.log("로그인 성공:", response.data);
+        //         localStorage.setItem("token",response.data.token);
+        //         navigate("/");
+        //     } catch (error) {
+        //         console.error(error);
+        //         alert("구글 로그인 실패");
+        //         navigate("/login");
+        //     }
+        // };
+
+        // fetchGoogleToken();
+    }, [location, navigate]);
+
+    return <div>구글 로그인 처리 중...</div>;
 }
