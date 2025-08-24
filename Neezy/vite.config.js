169f234c2fa31a38cd20039a1f32e3d64ef01608
윤현승 <yhs1144@naver.com>
2025-08-24 15:48:27 +0900

    Feat: vite.config.js 파일 수정

--- Neezy/vite.config.js
+++ Neezy/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/", // 커스텀 도메인 기준으로 기본 경로 설정 (루트 경로)
   plugins: [react()],
   server: {
     proxy: {
@@ -18,5 +19,8 @@ export default defineConfig({
         secure: true,
       },
     },
+    // 아래 옵션들은 필요에 따라 추가
+    // host: true, // 외부 접근 가능하게 할 때
+    // port: 5173, // 원하는 포트 설정
   },
 });
